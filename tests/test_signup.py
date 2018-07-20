ed7048e5fafccb54f61d973b1c8215a6a2d5d9a8
Darius-Ndubi <ndubidarius@gmail.com>
2018-07-20 15:40:32 +0300

    Adds more tests Adds a test for email validation Adds a test for empty username check Adds a test fro empty password check

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -12,6 +12,9 @@ createall_tables()
     The data will mock a real user
 """
 mock_reg={"email":"yagamidelight@gmail.com","username":"delight","password":"delight"}
+mock_reg1={"email":"","username":"delight","password":"delight"}
+mock_reg2={"email":"yagamidelight@gmail.com","username":"","password":"delight"}
+mock_reg3={"email":"yagamidelight@gmail.com","username":"delight","password":""}
 
 """
     A fuction to find users signed in
@@ -49,7 +52,7 @@ def test_signup():
 
 """
     A test on user signup 
-    the test tests dding of new user to the data base
+    the test tests adding of new user to the data base
 """
 def test_signup():
     old_num_users=registered()
@@ -63,3 +66,44 @@ def test_signup():
         assert(response.status_code==200)
 
 
+"""
+    A test to test if the email field is empty
+"""
+def test_signup():
+    #old_num_users=registered()
+    result=app.test_client()
+    response=result.post('/api/v1/auth/signup', data=json.dumps(mock_reg1),content_type='application/json')
+    #new_num_users=registered()
+    data_input=json.loads(response.data.decode('utf-8'))
+    if data_input.get('email')=="":
+    #assert(old_num_users==new_num_users)
+        assert response.json=={'Error': 'Email field cannot be empty'}
+        assert(response.status_code==401)
+
+"""
+    A test to test if username entered is empty
+"""
+def test_signup():
+    #old_num_users=registered()
+    result=app.test_client()
+    response=result.post('/api/v1/auth/signup', data=json.dumps(mock_reg2),content_type='application/json')
+    #new_num_users=registered()
+    data_input=json.loads(response.data.decode('utf-8'))
+    if data_input.get('username')=="":
+    #assert(old_num_users==new_num_users)
+        assert response.json=={"Error": "Username field cannot be empty"}
+        assert(response.status_code==401)
+
+"""
+    A test to test if password entered is empty
+"""
+def test_signup():
+    #old_num_users=registered()
+    result=app.test_client()
+    response=result.post('/api/v1/auth/signup', data=json.dumps(mock_reg3),content_type='application/json')
+    #new_num_users=registered()
+    data_input=json.loads(response.data.decode('utf-8'))
+    if data_input.get('password')=="":
+    #assert(old_num_users==new_num_users)
+        assert response.json=={"Error": "Password fields cannot be empty"}
+        assert(response.status_code==401)
